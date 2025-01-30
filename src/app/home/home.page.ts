1134233e6351d41163f8a76503a5005c0b428307
Alberto Pérez <19110@portalcedi.edu.mx>
2025-01-30 08:25:38 -0600

    sumatica
    lol

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NasaService } from '../nasa.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+imageData: any;
+fecha = new FormControl(new Date());
+
+  constructor(private nasaService: NasaService) {}
+
+  ngOnInit(){
+    this.nasaService.getImageOfTheDay().subscribe((data) => {
+      this.imageData = data;
+      console.log(data);
+    })
+  }
+
+  
 
 }
--- src/app/nasa.service.ts
+++ src/app/nasa.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class NasaService {
+  private apiUrl = 'https://api.nasa.gov/planetary/apod';
+  private apiKey = '9YgkUeMw5gUfjVxv7yCxQCGTPOE1Rwk1dWqm1kWh'; // Reemplaza con tu API Key
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  getImageOfTheDay(): Observable<any> {
+    return this.http.get(`${this.apiUrl}?api_key=${this.apiKey}`);
+  }
+
+  
+  
 }
